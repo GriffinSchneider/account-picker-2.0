05de92f8ed6738f6ed2c93b2e6e6520c7436d421
Griffin Schneider <griffinschneider@gmail.com>
2013-07-18 15:51:08 -0400

    Make 'like' case-insensitive under postgres

--- picker.rb
+++ picker.rb
@@ -3,6 +3,8 @@ require "sinatra"
 require "data_mapper"
 require "json"
 
+require './postgres-case-insensitive-like'
+
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{File.expand_path(File.dirname(__FILE__))}/accounts.db")
 class Account
     include DataMapper::Resource
